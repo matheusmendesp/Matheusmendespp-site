1fbf5c95c3eb6121ce3c966ca3f65bf38e29f752
Isa <matheusm@MacBook-Air-de-Matheus.local>
2023-11-08 18:56:13 -0300

    Portfolio commit

---
+++ src/content_option.js
@@ -0,0 +1,77 @@
+const logotext = "Matheus";
+
+const meta = {
+    title: "Matheus Mendes",
+    description: "",
+};
+
+const introdata = {
+    title: "I’m Matheus Mendes",
+    animated: {
+        first: "I develop mobile apps",
+    },
+    your_img_url: "https://scontent.fcgh51-1.fna.fbcdn.net/v/t39.30808-6/315767519_3393292084276840_1891412086178864305_n.jpg?stp=cp6_dst-jpg&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeF1DOe-CXPpySWO_iO70gQEN4D9AULCwvc3gP0BQsLC9_7QAHM4Y6K7yQQ6KDc_0obHORIhtNDawwI-eWf3jXye&_nc_ohc=aSIjECc2nLsAX9eECy0&_nc_ht=scontent.fcgh51-1.fna&oh=00_AfA3GR0DlpTG8WD1hs1Rv0rF9bCZJvyxNVqDJXJeDcAKQQ&oe=65507109",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I'm Matheus Mendes, 23 years old, and I'm a mobile app developer with two years of experience in iOS development. I also have skills in cross-platform technologies like React Native and Flutter, allowing me to create apps that work across different operating systems. I'm committed to staying updated with the latest trends in mobile app development, and I believe in the transformative power of technology. Outside of work, I stay active and work on personal coding projects to keep learning and evolving. I'm excited to take on new challenges and contribute to innovative projects in the future.",
+};
+
+const worktimeline = [
+    {
+        jobtitle: "IOS Developer",
+        where: "SKY",
+        date: "2021/2023",
+    },
+];
+
+const skills = [
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/swift/swift-original.svg",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/react/react-original.svg",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/jenkins/jenkins-original.svg",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/git/git-original.svg",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/javascript/javascript-plain.svg",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/firebase/firebase-plain.svg",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/flutter/flutter-plain.svg",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "matheusmendespedrop@gmail.com",
+    YOUR_FONE: "+55 (14) 99111-6365",
+    description: "Feel free to get in touch with me! If you have any questions, want to discuss a potential project, or just wish to connect, I'm here for a chat. ",
+    YOUR_SERVICE_ID: "service_xkwq38c",
+    YOUR_TEMPLATE_ID: "template_6clkbbb",
+    YOUR_USER_ID: "9nsNiPDI2soEbZ14j",
+};
+
+const socialprofils = {
+    github: "https://github.com/matheusmendesp",
+    linkedin: "https://www.linkedin.com/in/matheusmendespp/",
+    instagram: "https://www.instagram.com/matheus.mendespp/"
+};
+
+export {
+    meta,
+    dataabout,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
